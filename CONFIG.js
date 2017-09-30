6afb1413b40ec3dc8f3f9a432580f4698c37607d
bullwinkle <developer085@gmail.com>
2017-09-30 05:36:11 +0300

    config port

--- CONFIG.js
+++ CONFIG.js
@@ -2,8 +2,8 @@ const path = require("path");
 const root = process.cwd();
 
 module.exports = {
-	WEBSOCKET_PORT : '80',
-	WEBSERVER_PORT : '80',
+	WEBSOCKET_PORT : '3000',
+	WEBSERVER_PORT : '3000',
 	PATH : {
 		ROOT: root,
 		DIST: path.join(root,'dist'),
