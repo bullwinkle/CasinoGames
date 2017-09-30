d50a41fa2229d239638459771f6bbaefa6cd6df2
bullwinkle <developer085@gmail.com>
2017-09-30 07:40:54 +0300

    broadcasting all connections

--- CONFIG.js
+++ CONFIG.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const root = process.cwd();
-const PROD = process.env.NODE_ENV === 'production';
+const DEV = process.env.NODE_ENV === 'development';
 
 module.exports.PORT_HTTP = process.env.PORT || 3000;
 module.exports.PORT_WEBSOCKET = process.env.PORT || 3000;
 
-module.exports.CONNECT_URL_WEBSOCKET = PROD ? '/' : `//localhost:${module.exports.PORT_HTTP}`;
-module.exports.CONNECT_URL_HTTP = PROD ? '/' : `//localhost:${module.exports.PORT_WEBSOCKET}`;
+module.exports.CONNECT_URL_WEBSOCKET = DEV ? `//localhost:${module.exports.PORT_HTTP}` : '/' ;
+module.exports.CONNECT_URL_HTTP = DEV ? `//localhost:${module.exports.PORT_WEBSOCKET}` : '/' ;
 
 module.exports.PATH_ROOT = root;
 module.exports.PATH_DIST = path.join(root,'dist');
